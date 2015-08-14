1a1809b65af1dbde7bc969495caf66a2ca3a7cea
ShiZhan <g.shizhan.g@gmail.com>
2015-08-14 19:23:42 +0800

    project definition

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := "MillionMonkeys"
-
-version := "1.0"
-
-scalaVersion := "2.10.3"
--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,24 @@
 import sbt._
 import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object MyBuild extends Build {
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+  val scalaV = Option(System.getProperty("scala.version")).getOrElse("2.11.6")
+  lazy val commonSettings = Defaults.defaultSettings ++ Seq(
     version := "0.1-SNAPSHOT",
     organization := "com.simba",
-    scalaVersion := "2.10.3"
+    scalaVersion := scalaV
   )
+
+  lazy val buildSettings = commonSettings
 
   lazy val MillionMonkeys = Project(
     id = "MillionMonkeys",
     base = file("."),
-    settings = Defaults.defaultSettings ++
-    sbtassembly.Plugin.assemblySettings
+    settings = commonSettings ++ assemblySettings ++
+      Seq(
+        logLevel in assembly := Level.Warn
+      )
   )
 }
